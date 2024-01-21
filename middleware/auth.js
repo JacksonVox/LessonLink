9dcb1ff72de921ab1380d7054811d3dcb075c4bc
Jackson McDonald <jacksonmcdonalddev@gmail.com>
2024-01-20 19:32:09 -0800

    Add redirect for student users in ensureTeacher middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,6 +9,8 @@ module.exports = {
     ensureTeacher: function (req, res, next) {
       if (req.user && req.user.accountType === 'teacher') {
         return next();
+      } else if (req.user && req.user.accountType === 'student') {
+        res.redirect('/studentHome/' + req.user._id)
       } else {
         res.status(403).send('Unauthorized'); // User is not a teacher, so respond with 403 Unauthorized
       }
