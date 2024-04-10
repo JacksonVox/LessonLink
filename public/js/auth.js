95bb59106131d5957e7536080beaaf5f36917528
JacksonVox <jacksonmcdonalddev@gmail.com>
2024-04-09 21:38:08 -0700

    added autocomplete attributes and fixed signup bug lacking script link

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,6 +1,15 @@
+// Click listener for submit button
 document.querySelector('.btn-1').addEventListener('click', function(event) {
   event.preventDefault();
   let form = document.querySelector('form');
-  
   form.submit();
+});
+
+//keydown listener to submit form
+document.querySelector('form').addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    // Submit form
+    this.submit();
+  }
 });
